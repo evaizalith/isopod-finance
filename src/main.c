b44bd29ef0af6de975a8a479b3bcadd142031c6f
FoundIzalith <evapowlison@protonmail.com>
2024-11-23 11:27:20 -0500

    Add files

---
+++ src/main.c
@@ -0,0 +1,13 @@
+// isopod-finance
+// --------------
+// main.c
+// --------------
+
+#include <stdio.h>
+#include "db.h"
+#include "config.h"
+#include "args.h"
+
+int main(int agrc, char* argv[]) {
+    
+}
